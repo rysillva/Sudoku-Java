a8742969dab2aa6599239eaf7b35a5723343d30a
rysillva <ry.luis239@gmail.com>
2026-07-25 16:10:20 -0300

    Update Sudoku.java

--- src/Sudoku.java
+++ src/Sudoku.java
@@ -1,22 +1,17 @@
 public class Sudoku {
-
     private int[][] tabuleiro;
-
-    // Construtor padrão
     public Sudoku() {
         tabuleiro = new int[9][9];
     }
 
-    // Construtor que recebe uma matriz
     public Sudoku(int[][] matriz) {
-
         tabuleiro = new int[9][9];
-
+        
         for (int linha = 0; linha < 9; linha++) {
             for (int coluna = 0; coluna < 9; coluna++) {
                 tabuleiro[linha][coluna] = matriz[linha][coluna];
-            }
         }
+     }
     }
 
     public int getValor(int linha, int coluna) {
@@ -30,56 +25,41 @@ public class Sudoku {
     public int[][] getTabuleiro() {
         return tabuleiro;
     }
-
     public void limpar() {
-
         for (int linha = 0; linha < 9; linha++) {
             for (int coluna = 0; coluna < 9; coluna++) {
                 tabuleiro[linha][coluna] = 0;
             }
         }
-
     }
 
     public Sudoku copiar() {
-
         Sudoku copia = new Sudoku(tabuleiro);
-
         for (int linha = 0; linha < 9; linha++) {
             for (int coluna = 0; coluna < 9; coluna++) {
                 copia.setValor(linha, coluna, this.getValor(linha, coluna));
             }
         }
-
-        return copia;
+     return copia;
     }
-
     public boolean estaCompleto() {
-
         for (int linha = 0; linha < 9; linha++) {
             for (int coluna = 0; coluna < 9; coluna++) {
-
                 if (tabuleiro[linha][coluna] == 0) {
                     return false;
                 }
-
             }
         }
-
         return true;
     }
 
     public int[][] copiarTabuleiro() {
-
-        int[][] copia = new int[9][9];
-
-        for (int linha = 0; linha < 9; linha++) {
-            for (int coluna = 0; coluna < 9; coluna++) {
+          int[][] copia = new int[9][9];
+          for (int linha = 0; linha < 9; linha++) {
+          for (int coluna = 0; coluna < 9; coluna++) {
                 copia[linha][coluna] = tabuleiro[linha][coluna];
             }
         }
-
         return copia;
     }
-
 }
