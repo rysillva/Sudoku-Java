5802821a1724eb143e09d9b3c35d76ba76f943e4
rysillva <ry.luis239@gmail.com>
2026-07-25 14:36:57 -0300

    Add files via upload

---
+++ src/SudokuCell.java
@@ -0,0 +1,33 @@
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+
+import javax.swing.JTextField;
+
+public class SudokuCell extends JTextField {
+
+    public SudokuCell() {
+
+        configurarCelula();
+
+    }
+
+    private void configurarCelula() {
+
+        setHorizontalAlignment(JTextField.CENTER);
+
+        setFont(new Font("Segoe UI", Font.BOLD, 28));
+
+        setPreferredSize(new Dimension(60, 60));
+
+        setBackground(Color.WHITE);
+
+        setSelectionColor(new Color(120, 170, 255));
+
+        setCaretColor(Color.BLUE);
+
+        setDocument(new NumberDocument());
+
+    }
+
+}
