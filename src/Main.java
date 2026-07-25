530f0b9c9b2138eb8c3e75139d7f22c03641c73c
rysillva <ry.luis239@gmail.com>
2026-07-25 16:07:29 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,10 @@ import javax.swing.SwingUtilities;
 public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
-
-            @Override
-            public void run() {
+        @Override
+        public void run() {
                 new SudokuFrame();
-
-            }
-        });
-    }
+         }
+     });
+ }
 }
