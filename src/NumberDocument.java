59fc7574e7c1656ad0b6ff6f89bf8e0bf5459e3e
rysillva <ry.luis239@gmail.com>
2026-07-25 16:08:18 -0300

    Update NumberDocument.java

--- src/NumberDocument.java
+++ src/NumberDocument.java
@@ -3,25 +3,17 @@ import javax.swing.text.BadLocationException;
 import javax.swing.text.PlainDocument;
 
 public class NumberDocument extends PlainDocument {
-
     @Override
     public void insertString(int offset, String texto, AttributeSet atributo)
             throws BadLocationException {
-
         if (texto == null) {
             return;
         }
-
-        // Permite apenas um caractere
         if (getLength() >= 1) {
             return;
         }
-
-        // Aceita somente números de 1 a 9
         if (texto.matches("[1-9]")) {
             super.insertString(offset, texto, atributo);
-        }
-
-    }
-
+   }
+ }
 }
